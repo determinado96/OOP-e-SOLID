cf73de0119d3860471ed21a4fa90f8fc69ece801
rodirodrigorodrigues <mspdhuwqheuwms@gmail.com.bad>
2026-02-23 10:55:21 -0300

    fix: fix content files changed

--- OOP/01_structural-concepts/index.js
+++ OOP/01_structural-concepts/index.js
@@ -1,19 +1,28 @@
 class Personagem {
+    static universo = 'Mário Bros'; // Atributo estático da classe Personagem.
+
+    // Atributos de instância.
     constructor(nome, cor, quantidadeDeCogumelos, altura, tipoFisico, possuiBigode) {
-        this.nome = nome;
+        this._nome = nome;
         this.cor = cor;
         this.quantidadeDeCogumelos = quantidadeDeCogumelos;
         this.altura = altura;
         this.tipoFisico = tipoFisico;
         this.possuiBigode = possuiBigode;
     }
-
+    
+    // Métodos de instância.
     get nome() {
-        return this.nome;
+        return this._nome;
     }
 
     set nome(novoNome) {
-        this.nome = novoNome;
+        this._nome = novoNome;
+    }
+
+    // Método estático da classe Personagem.
+    static apresentarUniverso() {
+        console.log(`Todos os personagens pertencem ao universo ${Personagem.universo}`);
     }
 
     pular() {
@@ -32,3 +41,12 @@ class Personagem {
 
 // Obejtos devem ser nomeados com substantivos.
 const personagem = new Personagem('Mario', 'Vermelho', 0, 1.5, 'Atletico', true);
+const personagem2 = new Personagem('Luigi', 'Verde', 0, 1.8, 'Atletico', true);
+
+console.log(personagem);
+console.log(personagem2);
+
+// Mensagem
+Personagem.apresentarUniverso();
+personagem.pular();
+personagem.pegarCogumelo();
